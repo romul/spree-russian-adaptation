b7523c13f3e6ccb50e40990480d64ccc18ae3142
Roman Smirnov <POMAHC@gmail.com>
2009-08-17 14:20:37 +0400

    Move helper methods to RussianAdaptationExtension.activate

--- russian_adaptation_extension.rb
+++ russian_adaptation_extension.rb
@@ -32,8 +32,25 @@ class RussianAdaptationExtension < Spree::Extension
       end
     end
 
-    Spree::BaseController.class_eval do
-       helper RussianHelper
+    Spree::BaseHelper.module_eval do
+      def number_to_currency(number)
+        rub = number.to_i
+        kop = ((number - rub)*100).round.to_i
+        if (kop > 0)
+          "#{rub}&nbsp;p.&nbsp;#{'%.2d' % kop}&nbsp;коп."
+        else
+          "#{rub}&nbsp;p."
+        end
+      end
+
+      def text_area(object_name, method, options = {})
+        begin
+          fckeditor_textarea(object_name, method,
+            :toolbarSet => 'Easy', :width => '100%', :height => '350px')
+        rescue
+          super
+        end
+      end
     end
 
     # admin.tabs.add "Russian Adaptation", "/admin/russian_adaptation", :after => "Layouts", :visibility => [:all]
