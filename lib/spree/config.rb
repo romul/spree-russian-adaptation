7fc5719e8cc9fed9ca26ca99bd44deea9067cd9d
Roman Smirnov <POMAHC@gmail.com>
2009-09-03 13:19:44 +0400

    Устраняет лишние запросы к БД по поводу конфигурации.

---
+++ lib/spree/config.rb
@@ -0,0 +1,22 @@
+module Spree
+  # Singleton class to access the configuration object (AppConfiguration.first by default) and it's preferences.
+  #
+  # Usage:
+  #   Spree::Config[:foo]                  # Returns the +foo+ preference
+  #   Spree::Config[]                      # Returns a Hash with all the application preferences
+  #   Spree::Config.instance               # Returns the configuration object (AppConfiguration.first)
+  #   Spree::Config.set(preferences_hash)  # Set the application preferences as especified in +preference_hash+
+  class Config
+    include Singleton
+    include PreferenceAccess
+    
+    class << self
+      def instance
+        return @configuration if @configuration
+        return nil unless ActiveRecord::Base.connection.tables.include?('configurations')
+        @configuration ||= AppConfiguration.find_or_create_by_name("Default configuration")
+        @configuration
+      end
+    end
+  end
+end
