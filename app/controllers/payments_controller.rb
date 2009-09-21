50cc643292cbaf8358bf809103a50d16370fc2e6
Roman Smirnov <POMAHC@gmail.com>
2009-09-21 11:09:21 +0400

    Зачисление платежа от RoboKassa перенесено в более подходящее место.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,57 +2,47 @@ class PaymentsController < Spree::BaseController
   before_filter :load_robokassa
   
   def show
-    @order = Order.find_by_number(params[:id])
+    load_order_by_number(params[:id])
   end
 
   def result
     if @robo_kassa.result?(params)
-       @status_operation = @robo_kassa.state_operation(params)
-       @order = Order.find_by_number('R' + params[:InvId])
-       if (@status_operation[:state].to_i == 100)
-         payment = Payment.new(:amount => params[:OutSum])
-         payment.order = @order
-         if payment.save
-           render :text => "OK#{params[:InvId]}", :status => :ok
-         else
-           render :text => "Payment not saved", :status => :ok
-         end
-         
-       else
-         render :text => "Status <> 100, = #{@status_operation[:state]}", :status => :ok
-       end
-      
+      load_order_by_number('R' + params[:InvId])
+      render :text => "OK#{params[:InvId]}", :status => :ok 
     else
       render :text => "Signature is invalid", :status => :ok
-    end
-    
+    end 
   end
 
   def success
-    @order = Order.find_by_number('R' + params[:InvId])    
+    load_order_by_number('R' + params[:InvId])
     if @robo_kassa.success?(params)
-      flash[:notice] = 'Платёж принят, спасибо!'     
+      payment = Payment.new(:amount => params[:OutSum])
+      payment.order = @order
+      payment.save
+      flash[:notice] = 'Платёж принят, спасибо!'
     else
       flash[:error] = 'Платёж не прошёл проверку подписи.'
     end
-    redirect_to home_url unless @order
     redirect_to order_url(@order.number)
   end
   
   def fail
-    @order = Order.find_by_number('R' + params[:InvId])
+    load_order_by_number('R' + params[:InvId])
     flash[:error] = 'Платёж не завершен или отменён.'
-    redirect_to home_url unless @order
     redirect_to order_url(@order.number)
   end
 
-
-
-    
   private 
 
   def accurate_title
     "Платежи"
+  end
+  
+  def load_order_by_number(number)
+    @order = Order.find_by_number(number)
+    flash[:error] = "Заказ с номером #{number} не найден."
+    redirect_to root_url unless @order
   end
   
   def load_robokassa
