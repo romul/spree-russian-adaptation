1680a18a611f5e7084f5d97f81ac2e7ce151b958
krick <antipin.konstantin@googlemail.com>
2010-01-29 02:06:37 +0800

    Удаленоа установка свойства products_per_page. так как оно не специфично к расширению russian_adaptation и скорее всего должно быть установлено в расширении site.

--- config/initializers/russian_settings.rb
+++ config/initializers/russian_settings.rb
@@ -2,7 +2,6 @@ Time.zone = "Moscow"
 I18n.default_locale = :'ru-RU'
 
 if Spree::Config.instance
-  Spree::Config.set(:products_per_page => 5)
   Spree::Config.set(:default_locale => :'ru-RU')
   Spree::Config.set(:default_country_id => 168)
   Spree::Config.set(:auto_capture => false)
